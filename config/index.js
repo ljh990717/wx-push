c6d334c37ef6bac9769e2bc4fff234171c479b0c
ljh990717 <80892713+ljh990717@users.noreply.github.com>
2023-11-21 16:57:18 +0800

    111

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx35c3c6c556dcd5f8",
   appsecret: "aa785f51da78e1b8962667434f280b90",
   template_id: "sm25V3A72v7YFVk4XPT3Bg-330Vti9o24pSX-alAZxg",
-  touser: "omvFm6gQsW9XhOoHeYoGwxhCY-F0",
+  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "沈阳",
